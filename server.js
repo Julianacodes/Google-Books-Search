ba44165e709e5ec275557ba1f5dd1740078ee6ab
admininstrator <ploy3_98@yahoo.com>
2020-09-18 09:03:44 -0500

    deployed

--- server.js
+++ server.js
@@ -5,11 +5,13 @@ const app = express()
 const path = require("path")
 
  if (process.env.NODE_ENV === 'production') {
-    app.use(express.static("client/build"));
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, '../client/build/index.html'));
-      });
+    app.use(express.static("client/build")); 
+
+    
  }
+ app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 //parse the data to json
