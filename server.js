bf68440889afd54cc2a4acf8a9f5a0693585a652
Juliana Hernandez <juliana88h@gmail.com>
2020-09-17 23:28:31 -0700

    updated server. js

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ const app = express()
 const path = require("path")
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
-if (process.env.NODE_ENV === 'production') {
+//if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/build')));
     app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, '../client/build/index.html'));
       });
-  }
+//}
 
 //parse the data to json
 //middleware parsing creates req.body
