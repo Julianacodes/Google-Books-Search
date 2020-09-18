af5940c7e9803ebf16956b9a75db370c05329b1f
admininstrator <ploy3_98@yahoo.com>
2020-09-18 09:00:46 -0500

    deployed

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ const PORT = process.env.PORT || 3001
 const app = express()
 const path = require("path")
 
-//if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')));
+ if (process.env.NODE_ENV === 'production') {
+    app.use(express.static("client/build"));
     app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, '../client/build/index.html'));
       });
-//}
+ }
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 //parse the data to json
