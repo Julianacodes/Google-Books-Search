4c3f2aca67724b2033fc3226b6890eb72b1c7490
Juliana Hernandez <juliana88h@gmail.com>
2020-09-17 23:06:20 -0700

    added to server.js

--- server.js
+++ server.js
@@ -16,6 +16,9 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googleBooks")
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/build')));
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../client/build/index.html'));
+      });
   }
 
 app.listen(PORT,function(){
